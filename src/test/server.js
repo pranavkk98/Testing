5ff3f5977d0e15063166b3d7ee267f32502bb8c6
pranav <pranavkk98@github.com>
2022-03-02 11:28:18 +0530

    tests with jest

---
+++ src/test/server.js
@@ -0,0 +1,14 @@
+import {setupWorker} from 'msw'
+import {handlers} from './server-handlers'
+import {homepage} from '../../package.json'
+
+const fullUrl = new URL(homepage)
+
+const server = setupWorker(...handlers)
+
+server.start({
+  quiet: true,
+  serviceWorker: {
+    url: fullUrl.pathname + 'mockServiceWorker.js',
+  },
+})
